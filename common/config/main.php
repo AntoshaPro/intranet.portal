5ce49588eca46ca5987d14d6c1f6c66b49e3b18a
proskurin <proskurin@proskurin.local>
2016-01-28 17:23:00 +0300

    Signed-off-by: proskurin <proskurin@proskurin.local>

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,48 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'modules' => [
+        'social' => [
+            // the module class
+            'class' => 'kartik\social\Module',
+
+            // the global settings for the disqus widget
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // default settings
+            ],
+
+            // the global settings for the facebook plugins widget
+            'facebook' => [
+                'appId' => '1508089749495084',
+                'secret' => 'cace823e115b92526922f075c0955351',
+            ],
+
+            // the global settings for the google plugins widget
+            'google' => [
+                'clientId' => 'GOOGLE_API_CLIENT_ID',
+                'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+                'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+            ],
+
+            // the global settings for the google analytic plugin widget
+            'googleAnalytics' => [
+                'id' => 'TRACKING_ID',
+                'domain' => 'TRACKING_DOMAIN',
+            ],
+
+            // the global settings for the twitter plugin widget
+            'twitter' => [
+                'screenName' => 'TWITTER_SCREEN_NAME'
+            ],
+        ],
+        // your other modules
+    ],
+
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+
     ],
 ];
