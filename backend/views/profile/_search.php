16c46fda3f6b2c938e5b7979ae00ec7428220057
proskurin <proskurin@proskurin.local>
2016-02-01 16:43:57 +0300

    Signed-off-by: proskurin <proskurin@proskurin.local>

--- backend/views/profile/_search.php
+++ backend/views/profile/_search.php
@@ -1,11 +1,12 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
-/* @var $this yii\web\View */
-/* @var $model backend\models\search\ProfileSearch */
-/* @var $form yii\widgets\ActiveForm */
+use frontend\models\Profile;
+/**
+ * @var yii\web\View $this
+ * @var backend\models\search\ProfileSearch $model
+ * @var yii\widgets\ActiveForm $form
+ */
 ?>
 
 <div class="profile-search">
@@ -15,23 +16,22 @@ use yii\widgets\ActiveForm;
         'method' => 'get',
     ]); ?>
 
-    <?= $form->field($model, 'id') ?>
 
-    <?= $form->field($model, 'user_id') ?>
 
     <?= $form->field($model, 'first_name') ?>
 
-    <?= $form->field($model, 'second_name') ?>
-
     <?= $form->field($model, 'last_name') ?>
 
-    <?php // echo $form->field($model, 'birthdate') ?>
+    <?= $form->field($model, 'birthdate') ?>
 
-    <?php // echo $form->field($model, 'gender_id') ?>
+    <?= $form->field($model, 'gender_id')->dropDownList(Profile::getgenderList(),
+        [ 'prompt' => 'Please Choose One' ]);?>
 
     <?php // echo $form->field($model, 'created_at') ?>
 
     <?php // echo $form->field($model, 'updated_at') ?>
+
+    <?php // echo $form->field($model, 'user_id') ?>
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
